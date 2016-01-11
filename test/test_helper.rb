ac56b50dab0ae43d263a131a3b9ef699731c8e54
adamki <adajensen@gmail.com>
2016-01-11 14:04:35 -0700

    added simplecov badge

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,21 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'simplecov'
+SimpleCov.start 'rails' do
+    require 'simplecov-badge'
+    # add your normal SimpleCov configs
+    add_filter "/app/admin/"
+    # configure any options you want for SimpleCov::Formatter::BadgeFormatter
+    SimpleCov::Formatter::BadgeFormatter.generate_groups = true
+    SimpleCov::Formatter::BadgeFormatter.strength_foreground = true
+    SimpleCov::Formatter::BadgeFormatter.timestamp = true
+    # call SimpleCov::Formatter::BadgeFormatter after the normal HTMLFormatter
+    SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+        SimpleCov::Formatter::HTMLFormatter,
+        SimpleCov::Formatter::BadgeFormatter,
+    ]
+end
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
