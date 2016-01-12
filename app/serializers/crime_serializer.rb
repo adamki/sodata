cc5435770eefa251810edb3c1c1de4f333b2f260
adamki <adajensen@gmail.com>
2016-01-11 23:33:32 -0700

    very basic serialization of crimes

---
+++ app/serializers/crime_serializer.rb
@@ -0,0 +1,11 @@
+class CrimeSerializer < ActiveModel::Serializer
+  attributes :id, 
+             :date_reported,
+             :latitude,
+             :longitude,
+             :hundred_block_location,
+             :offense_description,
+             :offense_type,
+             :zone_beat
+  
+end
