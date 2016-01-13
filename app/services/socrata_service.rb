5d5962e69f71ae90ef11d4569d22d3c6b6d78b27
adamki <adajensen@gmail.com>
2016-01-12 23:54:07 -0700

    temporary leave to get master

--- app/services/socrata_service.rb
+++ app/services/socrata_service.rb
@@ -6,25 +6,7 @@ class SocrataService
   end
 
   def fetch_seattle_crimes
-    responses = client.get("yamw-xkh3", { "$limit" => 100 })
-    responses.each do |crime_data|
-      crime = Crime.find_or_create_by( date_reported: crime_data.date_reported )
-      crime.update_attributes(
-        date_reported:          crime_data.date_reported,
-        latitude:               crime_data.location.latitude,
-        longitude:              crime_data.location.longitude,
-        hundred_block_location: crime_data.hundred_block_location,
-        offense_description:    crime_data.summarized_offense_description,
-        offense_code:           crime_data.offense_code,
-        offense_type:           crime_data.offense_type,
-        zone_beat:              crime_data.zone_beat
-      )
-      Rails.logger.info("updated crimes with: #{crime.offense_description} at:#{Time.now}")
-
-      
-      crime.save
-      crime
-    end
+    response = client.get("yamw-xkh3", { "$limit" => 100 })
   end
 
   def self.print_text
