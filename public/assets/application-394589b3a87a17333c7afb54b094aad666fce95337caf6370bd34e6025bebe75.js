b4e18c740d259d0713dfbd6a0fc3c8f71e539c0e
adamki <adajensen@gmail.com>
2016-01-18 00:29:59 -0700

    hulk'd

--- public/assets/application-394589b3a87a17333c7afb54b094aad666fce95337caf6370bd34e6025bebe75.js
+++ public/assets/application-394589b3a87a17333c7afb54b094aad666fce95337caf6370bd34e6025bebe75.js
@@ -69744,8 +69744,10 @@ var renderGraph = function(results){
 };
 // hover the user card and bike card respectivley
 $(document).ready(function(){
+  staticAnimations();
   allowHover();
   setupModal();
+  deleteModal();
 })
 
 function allowHover(){
@@ -69766,6 +69768,27 @@ function setupModal(){
   })
 }
 
+function staticAnimations(){
+  $('.tiny.image.circular')
+    .transition('vertical flip in')
+  ;
+
+  $('.icon.bike')
+    .transition({
+      animation : 'jiggle',
+      duration  : 800,
+      interval  : 200
+    })
+  ;
+}
+
+function deleteModal(){
+  $(".delete.confirmation").on("click", function(){
+    $('.modal.delete-bike')
+      .modal('show')
+    ;
+  })
+}
 ;
 $(document).ready(function(){
   $('.special.cards .image').dimmer({
