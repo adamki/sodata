5bad87c54a7e7700347b55c1b7128e825b3158ad
adamki <adajensen@gmail.com>
2016-01-18 18:29:58 -0700

    changed dashboard controller to visitors from wwandering into dashboard#show

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def show
-    redirect_to root_path if !current_user
     @bikes = current_user.bikes
+    redirect_to root_path if !current_user
   end
 end
