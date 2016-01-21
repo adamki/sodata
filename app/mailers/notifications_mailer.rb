aa7cdf678bd33b873528b0db8958b544aa8cd29d
adamki <adajensen@gmail.com>
2016-01-21 10:45:52 -0700

    removed noticationsmailer call and tests are at 96.7

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -7,14 +7,4 @@ class NotificationsMailer < ApplicationMailer
     :subject => 'Welcome to Sodata!' )
   end
 
-  def send_stolen_alert(user, bike)
-    @sad_user = user
-    @bike = bike
-    @users = User.all
-    @users.each do |user|
-      @user = user
-      mail( :to => user.email,
-      :subject => 'ALERT! ALERT! STOLEN BIKE!' )
-    end
-  end
 end
