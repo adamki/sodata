b8f777c651ada4651815839f3d53c9384433627a
adamki <adajensen@gmail.com>
2016-05-07 21:53:03 -0700

    removed massive case statement for an exchange of .each loop that parses the date and returns the hour only. This is later used in charts.js and allows for the crime by hour to render properly.

--- app/services/socrata_service.rb
+++ app/services/socrata_service.rb
@@ -43,57 +43,11 @@ class SocrataService
   end
 
   def filter_date_reported(response_collection)
-    response_collection.each do |response|
-      case response[:date_reported][-8,2]
-      when "01"
-        response[:date_reported] = "1AM"
-      when "02"
-        response[:date_reported] = "2AM"
-      when "03"
-        response[:date_reported] = "3AM"
-      when "04"
-        response[:date_reported] = "4AM"
-      when "05"
-        response[:date_reported] = "5AM"
-      when "06"
-        response[:date_reported] = "6AM"
-      when "07"
-        response[:date_reported] = "7AM"
-      when "08"
-        response[:date_reported] = "8AM"
-      when "09"
-        response[:date_reported] = "9AM"
-      when "10"
-        response[:date_reported] = "10AM"
-      when "11"
-        response[:date_reported] = "11AM"
-      when "12"
-        response[:date_reported] = "12PM"
-      when "13"
-        response[:date_reported] = "1PM"
-      when "14"
-        response[:date_reported] = "2PM"
-      when "15"
-        response[:date_reported] = "3PM"
-      when "16"
-        response[:date_reported] = "4PM"
-      when "17"
-        response[:date_reported] = "5PM"
-      when "18"
-        response[:date_reported] = "6PM"
-      when "19"
-        response[:date_reported] = "7PM"
-      when "20"
-        response[:date_reported] = "8PM"
-      when "21"
-        response[:date_reported] = "9PM"
-      when "22"
-        response[:date_reported] = "10PM"
-      when "23"
-        response[:date_reported] = "11PM"
-      when "00"
-        response[:date_reported] = "12PM"
-      end
+    formatted_collection = response_collection.each do |response|
+      hour = DateTime.parse(response[:date_reported]).hour
+
+      response[:date_reported] = Time.parse("#{hour}:00").strftime("%l %P").delete(' ')
     end
   end
+
 end
